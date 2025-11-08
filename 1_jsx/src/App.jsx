5b22ad039a03e4836e2c8d766a2ff9990877f490
yadav sanket <yadavsanket262@gmail.com>
2025-11-08 11:16:08 +0530

    1

--- 1_jsx/src/App.jsx
+++ 1_jsx/src/App.jsx
@@ -11,7 +11,10 @@ let name="Sanket yadav";
       <h2>{10+10}</h2>
       <h3>{name}</h3>
       <h3>{car_obj.name}</h3>
-    </div>
+
+      <h3>Hello</h3> 
+      <h5>{34 +3 }</h5>   
+      </div>
     </>
   )
 }
